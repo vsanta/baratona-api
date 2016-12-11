57505b348202c0365f326eb9ada9cfcbd5372cfe
Vanessa Sant'Anna <pair+vsantanna@pivotallabs.com>
2016-12-11 14:39:49 -0500

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,12 @@ ActiveRecord::Schema.define(version: 20161210195503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
 end
