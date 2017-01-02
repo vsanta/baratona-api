e0862d235aba091f074d5fb0cd348cd30ef35132
Vanessa Sant'Anna <pair+vsantanna@pivotallabs.com>
2017-01-02 15:29:13 -0500

    Move lib to app so it autoloads

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module BaratonaApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.autoload_paths << Rails.root.join('lib')
   end
 end
